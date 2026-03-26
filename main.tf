767549dab80d21af33ad66835b88339bea76318a
s11yasir <yasiralrikabi@gmail.com>
2026-03-25 21:45:42 -0700

    Add vpc and subnet

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
 resource "aws_vpc" "my_vpc" {
+  provider = aws.east
   cidr_block = var.vpc_cidr
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  provider = aws.east
+  count = 2
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "var.vpc_cidr"
+  availability_zone = var.availability_zones[count.index]
+  tags = {
+    Name = "main-subnet"
+  }
+  
 }
